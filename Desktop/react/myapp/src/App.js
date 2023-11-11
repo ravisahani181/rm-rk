65793b0687a2826f41f3fdaf4c78d982cdbdd13c
ravi <rkprince573@gmail.com>
2023-11-11 13:41:26 +0530

    commit

---
+++ Desktop/react/myapp/src/App.js
@@ -0,0 +1,18 @@
+ import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        
+        <h1>  Welcome To Tech Master</h1>
+        i
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
